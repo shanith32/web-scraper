985647e74001e734410cb05c69d779cc845152c7
Nadika <nsbandara32@gmail.com>
2020-07-04 12:24:16 -0500

    Script to find all emails in the given url

---
+++ web-scraper-script.py
@@ -0,0 +1,16 @@
+import requests
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Find all emails in the given webpage')
+parser.add_argument('--url', required=True, help='Webpage url')
+args = parser.parse_args()
+
+url = args.url
+page = requests.get(url)
+email_regex = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+emails = re.finditer(email_regex, page.text)
+
+print('\nEmails found at ' + url + ':\n')
+for email in emails:
+    print(email.group())
